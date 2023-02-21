5bb26efe4a23e490a190e30fc5d83766a2f5974d
DaanTol <686379@student.inholland.nl>
2023-02-21 13:06:04 +0100

    specialchars added

--- app/controllers/homecontroller.php
+++ app/controllers/homecontroller.php
@@ -14,21 +14,23 @@ class HomeController extends Controller
     public function index()
     {
         $pages = $this->pageService->getContent();
+        foreach ($pages as &$encodedPage) {
+            $encodedPage->setBody_markup(htmlspecialchars_decode($encodedPage->getBody_markup()));
+        }
 
-        //$this->displayView(['pages' => $pages]);
-        require_once __DIR__ . '/../views/home/index.php';
+        $this->displayView($pages);
     }
 
     public function updateContent()
     {
-        $id = $_POST['id'];
-        $new_body_markup = $_POST['body_markup'];
+        $id = htmlspecialchars($_POST['id']);
+        $new_body_markup = htmlspecialchars($_POST['body_markup']);
 
         $result = $this->pageService->updateContent($id, $new_body_markup);
 
         if ($result) {
             // return succes response 
-            echo 'Update succesfull';
+            echo 'Update complete';
         } else {
             // return failed response
             echo 'Something went wrong with the update';
