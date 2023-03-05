decd1e83300d77e7f6e077fa88a6088847b30685
janjaap13 <janjaapvanlaar@gmail.com>
2023-03-05 23:39:24 +0100

    added dbconfig

---
+++ app/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "secret123";
+$database = "haarlem_festival";
+
+$hCaptchaSiteKey ="6372cb24-fcf5-4a3b-812e-d96ab9178747";
+$hCaptchaSecret ="0x116a7Fb45d2cFd0b7Ff3548b89bE1Ca1Be4319a6";
+
+$reCAPTCHASiteKeyV3 ="6Lfqj34kAAAAADDuQbtBwyopATqZLKr8n_gAqwSp";
+$reCAPTCHASecretV3 ="6Lfqj34kAAAAABFUsMdXTu9ZzHKW6x2PuXYo-IQR";
+
+?>
