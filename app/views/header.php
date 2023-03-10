bed9182ee84f01fb2d82636aaf8eabeaa4aac08e
janjaap13 <janjaapvanlaar@gmail.com>
2023-03-10 18:22:44 +0100

    reservation form

--- app/views/header.php
+++ app/views/header.php
@@ -41,38 +41,3 @@
       </div>
     </nav>
     <main class="container-fluid row align-items-center m-0 p-0 mb-auto h-100">
-
-
-
-
-    <form class="col-md-4 row bg-primary-b p-0" method="POST" id="history-ticket-form">
-  <fieldset class="form-group p-2 col-md-6 gx-100">
-    <label class="text-tetiare-a fs-5  col-form-label" for="name">Full name*</label>
-    <input class="form-control" type="text" id="reservation_name" name="reservation_name">
-  </fieldset>
-
-  <fieldset class="form-group p-2 col-md-3 gx-100">
-    <label class="text-tetiare-a fs-5 col-form-label" for="adults">Adults*</label>
-    <input class="form-control" type="text" id="adults" name="adults">
-  </fieldset>
-
-  <fieldset class="form-group p-2 col-md-3 gx-100">
-    <label class="text-tetiare-a fs-5 col-form-label" for="kids">Kids(<12)*</label>
-    <input class="form-control" type="text" id="kids" name="kids">
-  </fieldset>
-
-  <fieldset class="form-group p-2 col-md-8 gx-100">
-    <label class="text-tetiare-a fs-5 col-form-label" for="date">Reservation date*</label>
-    <input class="form-control" type="date" id="date" name="date">
-  </fieldset>
-
-  <fieldset class="form-group p-2 col-md-4 gx-100">
-    <label class="text-tetiare-a fs-5 col-form-label" for="time">Time*</label>
-    <input class="form-control" type="time" id="time" name="time">
-  </fieldset>
-
-
-
-  <input type="cancel" value="cancel" class="btn btn-secondary col-md-4">
-  <input type="submit" value="Submit" class="btn btn-primary col-md-8">
-</form>
