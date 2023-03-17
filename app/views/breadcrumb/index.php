9b114b403b758a9a029928d0c5d86237ac396f42
Floortje <florian.tjeertes@ziggo.nl>
2023-03-17 23:44:13 +0100

    update

--- app/views/breadcrumb/index.php
+++ app/views/breadcrumb/index.php
@@ -1,2 +1,6 @@
 <link rel="stylesheet" href="/css/breadcrumbs.css">
-<div class="container-fluid p-0 bg-primary-b "><i class="fa-solid fa-house"></i><p class="text-tetiare-a font-druk bread-crumb">breadcrumb</p></div>
+<div class="container-fluid p-0 bg-primary-b mt-0">
+    <i class="fa-solid fa-house text-tetiare-a align-bottom">
+        ><p class="inline"></p></i>
+    <p class="text-tetiare-a font-druk bread-crumb d-inline align-text-bottom"> breadcrumb</p>
+</div>
--- app/views/history/index.php
+++ app/views/history/index.php
@@ -10,33 +10,46 @@ new breadcrumbcontroller();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>History</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>History</title>
 </head>
 
-<body>
-    <input type="submit" class="btn btn-primary insert-button-venue" value="BUY TICKETS " style="width: 50%;">
+<body class="container">
+  <div class=" bg-tetiare-a">
+    <div class="col-6"><img></div>
+    <div class="col-6"><p> </p></div>
+  </div>
+  <div class="row bg-primary-b">
+    <div class="col-6"></div>
+    <div class="col-6"></div>
+  </div>
+  <div class="row bg-tetiare-a">
+    <div class="col-6"></div>
+    <div class="col-6"></div>
+  </div>
 
-   
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-  $(document).ready(function() {
-    // Hide the modal form by default
-    $('#history-ticket-form').hide();
-    
-    // Show the modal form when the button is clicked
-    $('.insert-button-venue').click(function() {
-      $('#history-ticket-form').show();
-    });
-    
-    // Hide the modal form when the "cancel" button is clicked
-    $('#history-ticket-form input[type="cancel"]').click(function() {
+  <input type="submit" class="btn btn-primary insert-button-venue" value="BUY TICKETS " style="width: 50%;">
+
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      // Hide the modal form by default
       $('#history-ticket-form').hide();
+
+      // Show the modal form when the button is clicked
+      $('.insert-button-venue').click(function() {
+        $('#history-ticket-form').show();
+      });
+
+      // Hide the modal form when the "cancel" button is clicked
+      $('#history-ticket-form input[type="cancel"]').click(function() {
+        $('#history-ticket-form').hide();
+      });
     });
-  });
-</script>
+  </script>
 
 </body>
 
