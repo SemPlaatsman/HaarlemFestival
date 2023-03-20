d03708b3b86b090cf12ac903eb7493c2f1059272
janjaap13 <janjaapvanlaar@gmail.com>
2023-03-20 13:40:30 +0100

    Update historycontroller.php

--- app/controllers/historycontroller.php
+++ app/controllers/historycontroller.php
@@ -38,23 +38,6 @@ class HistoryController extends Controller {
     public function insertItem()
     {
         try {
-            $id = 10;
-            $order_id = 2;
-            $event_id = 2;
-            $total_price = 1;
-            $VAT = 1;
-            $QR_Code = 1;
-            $item = new Item($id, $order_id, $event_id, $total_price, $VAT, $QR_Code);
-
-            $result = $this->itemService->deleteItem(10);
-
-            if ($result) {
-                // return success response
-                echo 'insert complete item';
-            } else {
-                // return failed response
-                echo 'Something went wrong with the insert';
-            }
         } catch (Exception $e) {
             echo 'Error: ' . $e->getMessage();
         }
