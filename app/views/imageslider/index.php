cbf328acf6a41415e51d0ea2555ad07088356161
Floortje <florian.tjeertes@ziggo.nl>
2023-03-21 23:41:16 +0100

    update

--- app/views/imageslider/index.php
+++ app/views/imageslider/index.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="/css/imageslider.css"></link>
-<div class="container-fluid h-50 p-0 mb-0">
+<div class="container-fluid imageslider p-0 mb-0">
     <img src="/img/png/history/slider/slider1.png" class="img w-100 h-100 slider" alt="lurem picsum images">
     </img><h1 class="carousel-caption  caption font-druktext text-tetiare-a">A STROLL THROUGH HISTORY</h1>
 </div>
