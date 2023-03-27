5eb7a2d8a04898edfb5a76e26311f0e738e4bdf7
DaanTol <686379@student.inholland.nl>
2023-03-27 23:45:59 +0200

    Everything works again, time to Krunker

--- app/models/artist.php
+++ app/models/artist.php
@@ -1,16 +1,18 @@
 <?php
-    class Artist {
+class Artist
+{
        public int $id;
        public string $name;
 
-       function __construct(int $id, string $name) {
-         $this->id = $id;
-         $this->name = $name;
+       function __construct(int $id, string $name)
+       {
+              $this->id = $id;
+              $this->name = $name;
        }
 
        /**
         * Get the value of id
-        */ 
+        */
        public function getId()
        {
               return $this->id;
@@ -18,9 +20,9 @@
 
        /**
         * Get the value of name
-        */ 
+        */
        public function getName()
        {
               return $this->name;
        }
-    }
+}
