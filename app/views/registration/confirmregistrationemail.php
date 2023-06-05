2f42625321d8bb15a826fb9f83ae1d7ed049ca3b
janjaap13 <janjaapvanlaar@gmail.com>
2023-06-05 14:31:39 +0200

    email fix

---
+++ app/views/registration/confirmregistrationemail.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Registration Complete</title>
+</head>
+<body>
+	<p>Dear user, <?php echo $name ?></p>
+	<p>Thank you for registering at the Haarlem Festival site.</p>
+	<p>You can login at:</p>
+	<p><a href="http://localhost">http://localhost</a></p>
+	<p>If you did not register at our site please contact us.</p>
+	<p>Kind regards,</p>
+	<p>The festival Team</p>
+</body>
+</html>
