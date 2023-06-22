499fecfc848f338ff7680b6f06b78022ffcee97d
janjaap13 <janjaapvanlaar@gmail.com>
2023-06-22 10:37:57 +0200

    endpoint to test sending tickets and small fix for sent ticket

---
+++ app/views/email/tickets.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ticket Confirmation</title>
+</head>
+<body>
+  <h1>Ticket Confirmation</h1>
+  
+  <p>Dear <?php echo $recipient ?>,</p>
+  
+  <p>Thank you for your reservation.</p>
+  
+  <p>If you have any questions or need further assistance, please feel free to contact us.</p>
+  
+  <p>Thank you again, and enjoy the event!</p>
+  
+  <p>Best regards,<br>
+  The Festival team</p>
+</body>
+</html>
+
